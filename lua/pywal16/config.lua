d3dbd99d256817620eb467d044704b49df0c12a2
R4ben <42911754+R4ben@users.noreply.github.com>
2024-12-07 17:11:46 -0100

    Update config.lua

--- lua/pywal16/config.lua
+++ lua/pywal16/config.lua
@@ -19,7 +19,7 @@ M.highlights_base = function(colors)
     Cursor = { fg = colors.foreground, bg = colors.cursor },
     CursorColumn = { bg = colors.transparent },
     CursorIM = { fg = colors.foreground, bg = colors.cursor },
-    CursorLine = { bg = colors.transparent },
+    CursorLine = { bg = colors.foreground },
     CursorLineNr = { fg = colors.color1 },
     Debug = { fg = colors.color11 },
     Define = { fg = colors.color6 },
